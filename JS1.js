7e3d954bbc6f7cfbe42c823bfcf3db3e16769453
vuminh98 <vuminhtran027@gmail.com>
2022-09-07 10:19:48 +0700

    message

---
+++ JS1.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Xin chao!');
+}
+function showMessage(){
+    document.getElementById('messageDiv').innerHTML='xin chao';
+}
+function showMessage(){
+    console.log('xin chao!');
+}
+function showMessage(){
+    document.write('xin chao!');
+}
